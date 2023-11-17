30cb6fdff4b230dfa8e9044423351aaef23e4420
Minseo Jo <87311912+Minseo-Jo@users.noreply.github.com>
2023-11-17 18:21:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ def scroll_handler():
 
 if __name__ == '__main__':
     model = joblib.load('rf_model.pkl')
-    app.run(host='0.0.0.0', port=443, debug=True, ssl_context=('cert.pem', 'key.pem'))
+    app.run(host='0.0.0.0', port=80, debug=True, ssl_context=('cert.pem', 'key.pem'))
 
 
 #app.run(host='0.0.0.0', port=80, debug=True, ssl_context=('path/to/cert.pem', 'path/to/key.pem'), ssl_version='TLSv1.2')
