40416a94bcea1d9bda53977c49c4384dc3e2e003
Minseo Jo <87311912+Minseo-Jo@users.noreply.github.com>
2024-04-07 23:28:07 +0900

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,3 @@
-# 가상환경 활성화
-# source venv_minseo/bin/activate 
-
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
@@ -11,7 +8,6 @@ from urllib.parse import quote
 import pandas as pd
 import numpy as np
 
-
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
@@ -19,16 +15,13 @@ from sklearn.preprocessing import RobustScaler
 from sklearn.impute import SimpleImputer
 import joblib
 import sklearn
-#from xgboost import XGBClassifier
 from catboost import CatBoostClassifier
-
 
 # 크롤링 모듈
 import naver_crawler
 
 app = Flask(__name__)
 CORS(app) 
-
 
 # 모델 예측을 위한 전처리
 def make_prediction(post_df) :
@@ -43,7 +36,6 @@ def make_prediction(post_df) :
     X_scaled = rbs.fit_transform(X)
     predict = model.predict(X_scaled)
     return predict
-
 
 @app.route("/naverblog", methods=['POST'])
 def recieve_data() :
@@ -75,6 +67,6 @@ def scroll_handler():
     return prediction
 
 if __name__ == '__main__':
-    model = joblib.load('modeling/model/catboost.pkl')        
-    app.run(host='0.0.0.0', debug=True)
+    model = joblib.load('model/catboost.pkl')        
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
